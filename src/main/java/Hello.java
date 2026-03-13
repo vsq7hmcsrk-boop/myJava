d42376e98defbc248d031204342f7f6d54ff7c62
dldbcks <ysyc54584@gmail.com>
2026-03-13 15:53:15 +0900

    Hello3 추가

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,4 +1,6 @@
-public class Hello {public static void main(String[] args){
-    System.out.println("Hello!! My Java Coding!!");
-System.out.println("edit Java code!!")}
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello!! My Java Coding!!");
+        System.out.println("edit Java code!!");
+    }
 }
