a58771ca9351b67385e24c297581268c687bae76
dldbcks <ysyc54584@gmail.com>
2026-03-13 15:37:54 +0900

    소스수정

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,2 +1,4 @@
-public class Hello {public static void main(String[] args){System.out.println("Hello!! My Java Coding!!");}
+public class Hello {public static void main(String[] args){
+    System.out.println("Hello!! My Java Coding!!");
+System.out.println("edit Java code!!")}
 }
