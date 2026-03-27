9fcc60bcf0d04f594ca0011bda9c64236dd8e13d
dldbcks <ysyc54584@gmail.com>
2026-03-27 13:42:52 +0900

    소스수정

---
+++ src/main/java/code03_9java.java
@@ -0,0 +1,12 @@
+public class code03_9java {
+    public static void main(String[] args) {
+        int num = 20;
+        num++ ; System.out.print(num+" ");
+        num-- ; System.out.print(num+" ");
+        num+- ; System.out.print(num+" ");
+        num*- ; System.out.print(num+" ");
+
+
+
+    }
+}
