6dc6db892ff7eccb520f420c0ea96e2802d2df3d
dldbcks <ysyc54584@gmail.com>
2026-03-27 13:29:46 +0900

    소스수정

---
+++ src/main/java/code03_5java.java
@@ -0,0 +1,7 @@
+public class code03_5java {
+    public static void main(String[] args) {
+        int a = 3, b = 4, c = 5;
+        System.out.println(a * b + c);
+        System.out.println(c + a * b);
+    }
+}
