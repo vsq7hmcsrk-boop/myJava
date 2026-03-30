1c3f05c0a0f99d5e9fe4f385303577c9cbbf85e5
dldbcks <ysyc54584@gmail.com>
2026-03-30 16:16:38 +0900

    소스수정

---
+++ src/main/java/Exam01.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Exam01 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        String a = s.nextLine();
+        String b = s.nextLine();
+
+        System.out.println((a + b).toLowerCase().replaceAll(" ", ""));
+
+        s.close();
+    }
+}
