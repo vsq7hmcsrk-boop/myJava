1539ace6f375f799f574b016de0d52ce0d1254f4
dldbcks <ysyc54584@gmail.com>
2026-03-31 09:26:13 +0900

    소스수정

---
+++ src/main/java/code4_14.java
@@ -0,0 +1,9 @@
+public class code4_14 {
+    public static void main(String[] args) {
+        String str = "난생처음 자바";
+
+
+        System.out.println(str.indexOf("난생"));
+        System.out.println(str.indexOf("처"));
+    }
+}
