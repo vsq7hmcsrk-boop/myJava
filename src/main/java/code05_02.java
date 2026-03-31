6780e014ba650b44e1acd46360f4efd0dbcbc8e9
dldbcks <ysyc54584@gmail.com>
2026-03-31 10:32:16 +0900

    소스수정

---
+++ src/main/java/code05_02.java
@@ -0,0 +1,10 @@
+public class code05_02 {
+    public static void main(String[] args) {
+        int num = 99;
+
+        if (num < 100) {
+            System.out.print("100보다 ");
+            System.out.print("작습니다.");
+        }
+    }
+}
