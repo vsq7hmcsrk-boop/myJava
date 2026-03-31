bc1f98ce273eff52d9b2937eea5e27d94dddc8d8
dldbcks <ysyc54584@gmail.com>
2026-03-31 10:53:52 +0900

    소스수정

---
+++ src/main/java/Exam3.java
@@ -0,0 +1,12 @@
+public class Exam3 {
+    public static void main(String[] args) {
+        int num = 2;
+
+        if (num < 3) {
+            System.out.println(".");
+            System.out.println("여기는 참입니다.");
+
+        }
+
+    }
+}
---
+++ src/main/java/code05_05.java
@@ -0,0 +1,2 @@
+public class code05_05 {
+}
