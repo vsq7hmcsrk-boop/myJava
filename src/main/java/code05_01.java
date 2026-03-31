64453cde33d51224a3311e5da773eec10002f6b0
dldbcks <ysyc54584@gmail.com>
2026-03-31 10:32:07 +0900

    소스수정

---
+++ src/main/java/code05_01.java
@@ -0,0 +1,9 @@
+public class code05_01 {
+    public static void main(String[] args) {
+        int num = 99;
+
+        if (num < 100) {
+            System.out.println("100보다 작습니다.");
+        }
+    }
+}
