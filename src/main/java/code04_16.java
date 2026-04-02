8764566245f987fe974f106493c6de2eb75de7f9
dldbcks <ysyc54584@gmail.com>
2026-04-02 19:13:39 +0900

    소스수정

---
+++ src/main/java/code04_16.java
@@ -0,0 +1,11 @@
+public class code04_16 {
+    public static void main(String[] args) {
+        String str = "Java";
+
+        System.out.println(str.charAt(0));
+        System.out.println(str.charAt(1));
+        System.out.println(str.charAt(2));
+        System.out.println(str.charAt(3));
+
+    }
+}
