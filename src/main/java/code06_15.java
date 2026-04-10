124d54383a18a1a39e7a504c8becc625de3c0664
dldbcks <ysyc54584@gmail.com>
2026-04-10 15:25:11 +0900

    소스수정

---
+++ src/main/java/code06_15.java
@@ -0,0 +1,11 @@
+public class code06_15 {
+    public static void main(String[] args) {
+        int hap = 0;
+        for (int i=1 ; i<100 ; i++) {
+            if (i % 4 == 0) {
+                hap += i;
+            }
+        }
+        System.out.println("1`100까지의 합계(4의 배수 제외) : " + hap);
+    }
+}
