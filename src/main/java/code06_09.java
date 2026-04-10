7bf317fc68706fa9b276d9b604cb12714aaeba6c
dldbcks <ysyc54584@gmail.com>
2026-04-10 14:38:50 +0900

    소스수정

--- src/main/java/code06_09.java
+++ src/main/java/code06_09.java
@@ -3,7 +3,7 @@ public class code06_09 {
 
         for (int i=0 ; i<3 ; i++) {
             for (int k=0 ; k<2 ; k++) {
-                System.out.println("난생처음은 쉽습니다.^^"+"(k:"+k+",k:"+k+")");
+                System.out.println("난생처음은 쉽습니다.^^"+"(i:"+i+",k:"+k+")");
             }
         }
 
