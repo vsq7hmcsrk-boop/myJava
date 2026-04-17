44a4abf504eed0df281568315480cc165a74cddc
dldbcks <ysyc54584@gmail.com>
2026-04-17 14:17:19 +0900

    소스수정

---
+++ src/main/java/code07_06.java
@@ -0,0 +1,16 @@
+class rabbit {
+    String shape;
+    int xPos;
+    int yPos;
+
+    void setPosition(int x, int y) {
+        xPos = x;
+        yPos = y;
+    }
+}
+
+public class code07_06 {
+    public static void main(String[] args) {
+
+    }
+}
